bee1f363a41e5d56e09c2c420bc9febcb1a7325b
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-01-13 16:51:23 -0500

    Fix Bug

--- vue_cdn/app.js
+++ vue_cdn/app.js
@@ -17,7 +17,7 @@ const app = Vue.createApp({
 
             console.log(results);
 
-            this.firstnmae = results[0].name.first
+            this.firstname = results[0].name.first
             this.lastname = results[0].name.last
             this.email = results[0].email
             this.gender = results[0].gender

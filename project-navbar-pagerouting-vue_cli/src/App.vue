3d9b0a15a28e53e0ff5305671e11b31ba042b11c
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-01-19 10:40:43 -0500

    app not working so using terminal but should just been name changes

---
+++ project-navbar-pagerouting-vue_cli/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <Nav> </Nav>
+
+    <router-view></router-view>
+    <!-- <PageViewer v-if="pages.length>0" :page="pages[activePage]"> </PageViewer>
+
+  <create-page @page-created="pageCreated"></create-page> -->
+</template>
+
+<script>
+    import Nav from './components/Nav.vue';
+
+    export default{
+        components: {
+            Nav
+        },
+        methods: {
+            pageCreated(pageObj) {
+                this.pages.push(pageObj);
+            }
+        }
+    }
+</script>
---
+++ project-navbar-pagerouting-vue_cli/src/views/PageViewer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-if="page"  class="container">
+        <h1>{{ page.pageTitle }}</h1>
+        <p>{{page.pageContent}}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    props:['index'],
+    inject:['$pages'],
+    created(){
+        this.page = this.$pages.getSinglePage(this.index)
+        // this.$watch(()=>this.$route.params,(newParams, prevParams)=>{
+        //     this.page = this.$pages.getSinglePage(newParams.index)
+        // })
+    },
+    data(){
+        return {
+            page: {
+                link : {text: "Home", url: "index.html"},
+                pageContent : "This is the home content",
+                pageTitle : "Home Page",
+                published : true
+            },
+        }
+    },
+    watch: {
+        index(newIndex, oldIndex){
+            this.page = this.$pages.getSinglePage(newIndex)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+h1{
+    margin-top: 30px !important;
+    margin-bottom: 30px !important;
+}
+
+
+
+</style>

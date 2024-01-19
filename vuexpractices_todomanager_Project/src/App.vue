d1e088f44b9d696eae26cb0c08ddf35edef86e4f
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-01-18 20:28:22 -0500

    Update App.vue

--- vuexpractices_todomanager_Project/src/App.vue
+++ vuexpractices_todomanager_Project/src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+
+// importing all components into main vue app
 import Todos from './components/Todos.vue';
 import AddTodo from './components/AddTodo.vue';
 import FilterTodos from './components/FiltersTodos.vue';
@@ -24,6 +26,7 @@ export default {
 </script>
 
 <style>
+/* css that will extend all throughout the application */
 body {
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif ;
   line-height: 1.6;
